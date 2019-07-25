8609c050aa22974dede7041e3db9cbbb33d842a8
Shubham Goyal <52886819+Shubham790@users.noreply.github.com>
2019-07-24 22:10:04 -0700

    Update findintersection.c

--- findintersection.c
+++ findintersection.c
@@ -1,27 +1,35 @@
-#include <stdio.h>
-int main()
-{
-int a[]={1,1,2,2,3,4,5,6};
-  int b[]={3,3,5};
-  int k=0;
-  int j=0;
-  int arr[3];
-  for(int i=0;i<8;i++)
-  {
-    if(b[j]==a[i])
+/**
+ * @input A : Read only ( DON'T MODIFY ) Integer array
+ * @input n1 : Integer array's ( A ) length
+ * @input B : Read only ( DON'T MODIFY ) Integer array
+ * @input n2 : Integer array's ( B ) length
+ * 
+ * @Output Integer array. You need to malloc memory, and fill the length in len1
+ */
+int* intersect(const int* A, int n1, const int* B, int n2, int *len1) {
+    int i=0,j=0,k=0;
+    int t=n1;
+  
+    int *arr=(int*) malloc(sizeof(int)*(n1));
+   // printf("%d",*len1);
+    while(i<n1 && j<n2)
     {
-      arr[k]=b[j];
-      j++;
-      k++;
+        if(A[i]==B[j])
+        {
+            arr[k]=B[j];
+            j++;
+            i++;
+            k++;
+        }
+        else if (B[j]<A[i])
+        {
+            j++;
+        }
+        else
+        {
+          i++;
+        }
     }
-    else if(b[j]<a[i])
-    {
-      j++;
-    }
-  }
-  for(int i=0;i<k;i++)
-  {
-    printf("%d ",arr[i]);
-  }
-    return 0;
+    *len1=k;
+    return arr;
 }
